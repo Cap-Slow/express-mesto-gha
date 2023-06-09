4c7e4c5b56d7d0484da0b67a443897901198056a
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-09 23:57:25 +0400

    feat: add error codes and eror messages constances

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+const BAD_REQUEST_CODE = 400;
+const NOT_FOUND_CODE = 404;
+const SERVER_ERROR_CODE = 500;
+const SERVER_ERROR_MESSAGE = 'На сервере произошла ошибка';
+const NOT_FOUND_USERID = 'Пользователь с указанным id не найден.';
+const NOT_FOUND_CARDID = 'Карточка с указанным id не найдена.';
+module.exports = {
+  BAD_REQUEST_CODE,
+  NOT_FOUND_CODE,
+  SERVER_ERROR_CODE,
+  SERVER_ERROR_MESSAGE,
+  NOT_FOUND_USERID,
+  NOT_FOUND_CARDID,
+};
