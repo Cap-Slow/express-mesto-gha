742e16231c8d67f896f2e36acbdbceaa44fc17a8
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-10 00:44:05 +0400

    feat: add bad request user and card messages

--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,8 @@ const BAD_REQUEST_CODE = 400;
 const NOT_FOUND_CODE = 404;
 const SERVER_ERROR_CODE = 500;
 const SERVER_ERROR_MESSAGE = 'На сервере произошла ошибка';
+const BAD_REQUEST_USER_MESSAGE = 'Передан некорректный id пользователя.';
+const BAD_REQUEST_CARD_MESSAGE = 'Передан некорректный id карточки.';
 const NOT_FOUND_USERID = 'Пользователь с указанным id не найден.';
 const NOT_FOUND_CARDID = 'Карточка с указанным id не найдена.';
 const NOT_FOUND_ROUTE = 'Запрашиваемый ресурс не найден';
@@ -10,6 +12,8 @@ module.exports = {
   NOT_FOUND_CODE,
   SERVER_ERROR_CODE,
   SERVER_ERROR_MESSAGE,
+  BAD_REQUEST_USER_MESSAGE,
+  BAD_REQUEST_CARD_MESSAGE,
   NOT_FOUND_USERID,
   NOT_FOUND_CARDID,
   NOT_FOUND_ROUTE,
