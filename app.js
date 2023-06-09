c2e795d16828b33aaebf286a6993ce9a2d5b5d6a
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-09 17:49:46 +0400

    feat: plug in bodyParser and connect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const { PORT = 3000 } = process.env;
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.json());
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
