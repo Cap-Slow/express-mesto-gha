0dbe20504a5ea23898061da88c5b11fdcc48a37c
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-10 00:14:22 +0400

    fix: add non-existent route handling logic

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const routes = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
+const { NOT_FOUND_CODE, NOT_FOUND_ROUTE } = require('../utils/constants');
 
 routes.use(userRoutes);
 routes.use(cardRoutes);
+routes.use('*', (req, res) => {
+  res.status(NOT_FOUND_CODE).send({ message: NOT_FOUND_ROUTE });
+});
 
 module.exports = routes;
--- utils/constants.js
+++ utils/constants.js
@@ -4,6 +4,7 @@ const SERVER_ERROR_CODE = 500;
 const SERVER_ERROR_MESSAGE = 'На сервере произошла ошибка';
 const NOT_FOUND_USERID = 'Пользователь с указанным id не найден.';
 const NOT_FOUND_CARDID = 'Карточка с указанным id не найдена.';
+const NOT_FOUND_ROUTE = 'Запрашиваемый ресурс не найден';
 module.exports = {
   BAD_REQUEST_CODE,
   NOT_FOUND_CODE,
@@ -11,4 +12,5 @@ module.exports = {
   SERVER_ERROR_MESSAGE,
   NOT_FOUND_USERID,
   NOT_FOUND_CARDID,
+  NOT_FOUND_ROUTE,
 };
