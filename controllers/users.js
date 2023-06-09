caabb1d9d6236f0a0eca18e8b29928afb93ae361
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-09 18:29:09 +0400

    feat: add user controller functions

---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+
+function getUsers(req, res) {
+  return User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+}
+
+function getUserById(req, res) {
+  const { Id } = req.params;
+  return User.findById(Id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      return res.status(200).send(user);
+    })
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+}
+
+function createUser(req, res) {
+  const { name, about, avatar } = req.body;
+  return User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+}
