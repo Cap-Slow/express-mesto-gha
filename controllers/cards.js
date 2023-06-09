ae604a70feadd61932384ebdb9cc687697a2a885
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-09 18:55:05 +0400

    feat: add getCards, createCard, deleteCard controllers

---
+++ controllers/cards.js
@@ -0,0 +1,27 @@
+const Card = require('../models/card');
+
+function getCards(req, res) {
+  return Card.find({})
+    .then((cards) => res.status(200).send(cards))
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+}
+
+function createCard(req, res) {
+  const { name, link } = req.body;
+  return Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.status(201).send(card))
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+}
+
+function deleteCard(req, res) {
+  const { cardId } = req.params;
+  return Card.findByIdAndRemove(cardId)
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Нет карточки с таким id' });
+        return;
+      }
+      return res.status(200).send({ message: 'Карточка удалена' });
+    })
+    .catch(() => res.status(500).send({ message: 'Ошибка сервера' }));
+}
