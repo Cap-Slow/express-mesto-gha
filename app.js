0f607336dd68bd08c71b78f2bb1621fca346d93e
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-09 18:12:31 +0400

    refactor: change the order of variable declarations

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
-const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mestodb');
+const { PORT = 3000 } = process.env;
+mongoose
+  .connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  });
+
+const app = express();
 app.use(bodyParser.json());
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
