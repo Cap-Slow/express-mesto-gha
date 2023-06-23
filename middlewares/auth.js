81ac75a620357652de463932ef34b4599d70a0aa
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-23 18:36:33 +0400

    refactor: check token inside middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,25 @@
-const { verifyToken } = require('../utils/jwt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 const {
   UNAUTHORIZED_CODE,
   UNAUTHORIZED_MESSAGE,
+  JWT_SECRET,
 } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
-  if (!verifyToken(req.cookies.jwt)) {
-    res.status(UNAUTHORIZED_CODE).send({ message: UNAUTHORIZED_MESSAGE });
-    return;
-  }
+  const token = req.cookies.jwt;
+  jwt.verify(token, JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return false;
+    }
+    const isUserExist = User.findById(decoded._id).then((user) => {
+      return Boolean(user);
+    });
+    if (!isUserExist) {
+      res.status(UNAUTHORIZED_CODE).send({ message: UNAUTHORIZED_MESSAGE });
+      return;
+    }
+    req.user._id = decoded._id;
+  });
   next();
 };
