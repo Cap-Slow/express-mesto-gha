88edc99f781a73dd80d8845b30c265f5088caf40
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-23 14:48:39 +0400

    feat: change route paths

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { NOT_FOUND_CODE, NOT_FOUND_ROUTE } = require('../utils/constants');
 
-routes.use('/users', userRoutes);
+routes.use('', userRoutes);
 routes.use('/cards', cardRoutes);
 routes.use('*', (req, res) => {
   res.status(NOT_FOUND_CODE).send({ message: NOT_FOUND_ROUTE });
