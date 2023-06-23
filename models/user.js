cd7b36e9b798dbfdc0ae782de803cacdca6f472e
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-23 13:18:49 +0400

    feat: add default values for name, about, avatar fields

--- models/user.js
+++ models/user.js
@@ -4,27 +4,25 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
     required: true,
     unique: true,
     validate: {
-      validator: (v) => validator.isEmail(v),
-      message: 'Неправильный формат почты',
+      validator: (email) => validator.isEmail(email),
+      message: 'Неверный формат почты',
     },
   },
   password: {
