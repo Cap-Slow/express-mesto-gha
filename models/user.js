228fdda5cdaa3fa51cca91c4f2eb1c014c5f98f8
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-23 20:07:31 +0400

    feat: hide the password from api response

--- models/user.js
+++ models/user.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    // select: false,
+    select: false,
   },
 });
 module.exports = mongoose.model('user', userSchema);
