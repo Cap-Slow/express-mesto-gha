6bf51b48f65909b9f550f631f7e79d04c312c3e5
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-25 02:14:20 +0400

    feat: handle validation errors with celebrate in card routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,5 @@
 const cardRoutes = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   createCard,
@@ -9,9 +9,44 @@ const {
 } = require('../controllers/cards');
 
 cardRoutes.get('', getCards);
-cardRoutes.post('', createCard);
-cardRoutes.delete('/:cardId', deleteCard);
-cardRoutes.put('/:cardId/likes', addCardLike);
-cardRoutes.delete('/:cardId/likes', removeCardLike);
+cardRoutes.post(
+  '',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string()
+        .required()
+        .pattern(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*\/?$/i),
+    }),
+  }),
+  createCard
+);
+cardRoutes.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  deleteCard
+);
+cardRoutes.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  addCardLike
+);
+cardRoutes.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  removeCardLike
+);
 
 module.exports = cardRoutes;
