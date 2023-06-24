d705a05e64fa877fcc8f1df0dc5be37f6bdcb293
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-25 02:11:54 +0400

    feat: connect handle errors middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const handleErrors = require('./middlewares/handleErrors');
 
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -13,4 +14,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routes);
+app.use(handleErrors);
+
 app.listen(PORT);
