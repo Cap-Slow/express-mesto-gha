cbffb800df302ce95e624737ae09558e35cf3b9a
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-25 02:14:47 +0400

    feat: handle validation errors with celebrate in user routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const userRoutes = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers,
   getUserById,
@@ -10,12 +11,77 @@ const {
 } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 
-userRoutes.post('/signup', createUser);
-userRoutes.post('/signin', login);
+userRoutes.post(
+  '/signup',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required(),
+      })
+      .unknown(true),
+  }),
+  createUser
+);
+
+userRoutes.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+
 userRoutes.get('/users', auth, getUsers);
-userRoutes.get('/users/:userId', auth, getUserById);
-userRoutes.patch('/users/me', auth, decoratedUpdateProfile);
-userRoutes.patch('/users/me/avatar', auth, decoratedUpdateAvatar);
-userRoutes.get('/users/me', auth, getUserInfo);
+
+userRoutes.get(
+  '/users/me',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().hex().length(24),
+    }),
+  }),
+  auth,
+  getUserInfo
+);
+
+userRoutes.get(
+  '/users/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().hex().length(24),
+    }),
+  }),
+  auth,
+  getUserById
+);
+
+userRoutes.patch(
+  '/users/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+    }),
+  }),
+  auth,
+  decoratedUpdateProfile
+);
+
+userRoutes.patch(
+  '/users/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .pattern(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*\/?$/i),
+    }),
+  }),
+  auth,
+  decoratedUpdateAvatar
+);
 
 module.exports = userRoutes;
