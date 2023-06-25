9ff7e387feea25f3bcb01fbb1ef31e3aca081056
Dmitrii Liadov <d.liadov@xsolla.com>
2023-06-25 16:20:14 +0400

    fix: make name and about not required fields

--- routes/users.js
+++ routes/users.js
@@ -18,8 +18,8 @@ userRoutes.post(
       .keys({
         email: Joi.string().required().email(),
         password: Joi.string().required(),
-        name: Joi.string().required().min(2).max(30),
-        about: Joi.string().required().min(2).max(30),
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
         avatar: Joi.string().pattern(
           /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*\/?$/i
         ),
